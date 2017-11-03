4479bd7263142bcdae309c573a126bdafa7644d1
Yana Ustsinovich <yustinovich@icloud.com>
2017-11-03 04:40:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,13 @@
 
 def turn_count(board)
 counter = 0
-board.each do |count|
-  if count == "X" || count == "O"
+  board.each do |count|
+   if count == "X" || count == "O"
     counter += 1
-
+   end
   end
+return counter
 end
-  return counter
+
+def current_player(board)
 end
